d7be9b72b6e68a4e782164acb4976028aabcd7ac
Ayokunnumi247 <163436878+Ayokunnumi247@users.noreply.github.com>
2024-05-07 20:31:42 +0100

    Added the AI API integration

--- src/script.js
+++ src/script.js
@@ -1,11 +1,27 @@
-function aiGenerator(event) {
-  event.preventDefault();
+function aiGenerator(response) {
+  console.log("recipe generated");
   new Typewriter("#information", {
-    strings: ["the recipt wil go here"],
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
 }
-let answerElement = document.querySelector("#recipt-form");
-answerElement.addEventListener("submit", aiGenerator);
+
+function generateRecipes(event) {
+  event.preventDefault();
+  let generalInput = document.querySelector("#input-data");
+  let apiKey = "8e4efa33a280aof6c33fe6a0t3ab54ec";
+  let context =
+    "please make it short and in a form of a list of things to do one after the other, not more than 4 lines";
+  let prompt = `you are an amazing AI that knows different type of food recipes around the world,generate food recipes of ${generalInput.value} `;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  console.log("generating recipe");
+  console.log(`prompt:${prompt}`);
+  console.log(`context:${context}`);
+
+  axios.get(apiUrl).then(aiGenerator);
+}
+
+let answerElement = document.querySelector("#recipe-form");
+answerElement.addEventListener("submit", generateRecipes);
