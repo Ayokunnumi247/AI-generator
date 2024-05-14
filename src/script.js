72a7eab40a123fe1d989b62c5e1502783dfa98ad
Ayokunnumi247 <163436878+Ayokunnumi247@users.noreply.github.com>
2024-05-14 11:53:51 +0100

    improved the user experience

--- src/script.js
+++ src/script.js
@@ -1,5 +1,4 @@
 function aiGenerator(response) {
-  console.log("recipe generated");
   new Typewriter("#information", {
     strings: response.data.answer,
     autoStart: true,
@@ -16,11 +15,11 @@ function generateRecipes(event) {
     "please make it short and in a form of a list of things to do one after the other, not more than 4 lines";
   let prompt = `you are an amazing AI that knows different type of food recipes around the world,generate food recipes of ${generalInput.value} `;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  console.log("generating recipe");
-  console.log(`prompt:${prompt}`);
-  console.log(`context:${context}`);
 
   axios.get(apiUrl).then(aiGenerator);
+  let sectionElement = document.querySelector("#information");
+  sectionElement.classList.remove("hidden");
+  sectionElement.innerHTML = `<div class="blink">⌛Generating Recipe for ${generalInput.value}⌛</div>`;
 }
 
 let answerElement = document.querySelector("#recipe-form");
